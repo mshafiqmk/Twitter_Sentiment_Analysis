46a9008b856d4b8cf4fdedb13223d586dfd90b3f
amansingh9097 <amansingh9097@gmail.com>
2018-03-24 17:52:37 +0530

    added template. updated gitignore

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
 from flask import Flask, request, render_template, jsonify
-from twitter_api import twitterClient
+from twitter import TwitterClient
 
 app = Flask(__name__)
-
 # Setup the client <query string, retweets_only bool, with_sentiment bool>
-api = twitterClient('Trump')
+api = TwitterClient('@yum_dude')
+
 
 def strtobool(v):
     return v.lower() in ["yes", "true", "t", "1"]
 
+
 @app.route('/')
 def index():
+
     return render_template('index.html')
+
 
 @app.route('/tweets')
 def tweets():
-    retweets_only = request.args.get('retweets_only')
-    api.set_retweets_checking(strtobool(retweets_only.lower()))
+        retweets_only = request.args.get('retweets_only')
+        api.set_retweet_checking(strtobool(retweets_only.lower()))
+        with_sentiment = request.args.get('with_sentiment')
+        api.set_with_sentiment(strtobool(with_sentiment.lower()))
+        query = request.args.get('query')
+        api.set_query(query)
 
-    with_sentiment = request.args.get('with_sentiment')
-    api.set_with_sentiment(strtobool(with_sentiment.lower()))
+        tweets = api.get_tweets()
+        return jsonify({'data': tweets, 'count': len(tweets)})
 
-    query = request.args.get('query')
-    api.set_query(query)
 
-    tweets = api.get_tweets()
-    return jsonify({'data': tweets, 'count': len(tweets)})
-
-app.run(host="0.0.0.0",port=5000,debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
